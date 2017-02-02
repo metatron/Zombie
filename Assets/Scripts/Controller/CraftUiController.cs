4aa361279a53925796106e53d14a61736fd5e168
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2017-02-03 00:42:19 +0900

    UIコントローラ設置

---
+++ Assets/Scripts/Controller/CraftUiController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TouchScript;
+
+public class CraftUiController : SingletonMonoBehaviourFast<CraftUiController> {
+	public GameObject craftingPanel;
+
+
+	public void OnOpenCraftMenuPressed() {
+		craftingPanel.SetActive (true);
+	}
+
+	public void OnCloseCraftMenuPressed() {
+		craftingPanel.SetActive (false);
+	}
+
+}
