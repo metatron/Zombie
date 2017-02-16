fb645f61a0644a142959e7c594504a005ebe165b
horikawa <horikawa.yoji.0156@colopl.co.jp>
2017-02-16 14:00:30 +0900

    added close and create button on CreatePanel.

--- Assets/Scripts/Ui/CreatePanel.cs
+++ Assets/Scripts/Ui/CreatePanel.cs
@@ -12,16 +12,38 @@ public class CreatePanel : MonoBehaviour {
 	public MaterialItem materialItemPrefab;
 
 	public void InitItemCraftingData(AbstractData itemData) {
+		gameObject.SetActive (true);
 		_itemData = itemData;
 
 		if (_itemData.GetType () == typeof(SwordData)) {
+			itemImage.sprite = GameManager.Instance.GetSpriteFromPath ("WeaponAtlas", _itemData.Image);
+			//アスペクトをONのまま横サイズをFixさせながらリサイズ。
+			itemImage.preserveAspect = true;
+			itemImage.SetNativeSize ();
+			Vector2 imgSize = itemImage.GetComponent<RectTransform>().sizeDelta;
+			imgSize.x = 100;
+			itemImage.GetComponent<RectTransform> ().sizeDelta = imgSize;
+
 			Dictionary<AbstractData.DataType, int> requirementDict = ((SwordData)_itemData).ParseRequirementStr ();
 			foreach (AbstractData.DataType dataType in requirementDict.Keys) {
 				int value = requirementDict [dataType];
 				MaterialItem materialItem = (MaterialItem)Instantiate (materialItemPrefab);
 				materialItem.materialNum.text = dataType + " x" + value;
-				materialItem.image.sprite = GameManager.Instance.GetSpriteFromPath ("WeaponAtlas", itemData.Image);
+				materialItem.transform.SetParent (MaterialListPos);
+				materialItem.transform.localPosition = Vector3.zero;
+				materialItem.transform.localScale = Vector3.one;
 			}
 		}
+	}
+
+	public void CloseCreatePanel() {
+		MaterialItem[] materialObjectArray = MaterialListPos.GetComponentsInChildren<MaterialItem> ();
+		int count = materialObjectArray.Length;
+		for (int i = 0; i < count; i++) {
+			DestroyImmediate (materialObjectArray [i].gameObject);
+		}
+		itemImage.sprite = null;
+
+		gameObject.SetActive (false);
 	}
 }
