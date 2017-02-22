11ab5eb20c77a01d63e6f040a822531465c555c0
horikawa <horikawa.yoji.0156@colopl.co.jp>
2017-02-22 14:11:27 +0900

    added female npc

---
+++ Assets/Scripts/Data/Data/CharaData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaData {
+	enum Gender: int {
+		Male, 
+		Female
+	}
+
+	enum WeaponType: int {
+		Sword,
+		Gun
+	}
+
+	public float HpBase { get; private set; }
+	public float hpCrnt = 10.0f;
+
+
+	public float AtkBase { get; private set; }
+	public float atkCrnt = 10.0f;
+
+	//次アタックするのに要する時間
+	public float atkInterval = 2.0f;
+
+	public float hanger = 100.0f;
+
+	public Gender gender = Gender.Male;
+	public WeaponType favoriteWpn = WeaponType.Sword;
+
+
+
+}
