24f8858b8edf98cfdce4665f703e20b57e152c4b
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2017-04-18 21:49:14 +0900

    ステージにNPC追加

--- Assets/Scripts/Data/Data/StageData.cs
+++ Assets/Scripts/Data/Data/StageData.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class StageData : AbstractData {
 	public string BG { get; private set; }
-	public string Spawn1 { get; private set; }
+	public string Spawn1 { get; private set; }		//Enemy01|HP:3|SPEED:1|INTERVAL:1.5
 	public string Spawn2 { get; private set; }
 	public string Spawn3 { get; private set; }
 	public float TimeTillBoss { get; private set; }
-	public string SpawnBoss { get; private set; }
-	public string Drops { get; private set; }
+	public string SpawnBoss { get; private set; }	//Enemy01|HP:30|SPEED:1
+	public string Drops { get; private set; }		//Metal:0.01|Wood:0.01
+	public string Npc { get; private set; }			//Rarity:Gender
 
 	/**
 	 * 
