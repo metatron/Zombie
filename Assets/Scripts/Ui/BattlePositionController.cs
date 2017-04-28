46a6b83fd561ffd1a8fd2b47385017c05f85b611
horikawa <horikawa.yoji.0156@colopl.co.jp>
2017-04-28 23:35:26 +0900

    modifying battle position logic

---
+++ Assets/Scripts/Ui/BattlePositionController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlePositionController : MonoBehaviour {
+
+	private void InitButtons() {
+		//まだWallを設置していない箇所はdisable
+		//既に他のNPCがとってる箇所はdisable
+	}
+}
