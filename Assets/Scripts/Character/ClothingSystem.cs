41c405b3e1ced9222f3ab4a6523d885271b9bee4
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2017-05-25 00:22:45 +0900

    creating clothing system.

---
+++ Assets/Scripts/Character/ClothingSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothingSystem : MonoBehaviour {
+	public enum BaseParts : int {
+		HEAD,			//0
+		BODY,
+		ARM_L,
+		ARM_R,
+		HIP,
+		LEG_UPPER_L,     //5
+		LEG_UPPER_R,
+		LEG_LOWER_L,
+		LEG_LOWER_R      //8
+	}
+
+	//服を置くベースとなるボーンのリスト
+	public List<GameObject> BodyBaseList = new List<GameObject>();
+}
